c957bcf851195e3193eeec5ce9027373d66103d3
junkoike0204 <50042012+junkoike0204@users.noreply.github.com>
2019-05-18 23:32:51 +0900

    リープモーションのスワイプを加えました。
    右方向にスワイプするとそれを認識してシーンが切り替わる様にしました。
    ただしシーン３と両立するには重かったため、シーン３を削った状態でスワイプは実装しました。

---
+++ ofApp.cpp
@@ -0,0 +1,183 @@
+#include "ofApp.h"
+#include "ofxLeapMotion2.h"
+//--------------------------------------------------------------
+void ofApp::setup(){
+    leap.open();
+    leap.setupGestures();
+    ofSetFrameRate(30);
+    LeapCon.enableGesture(Leap::Gesture::TYPE_SWIPE);
+    float microseconds = gesture.duration();
+    float seconds = gesture.durationSeconds();
+    //this sentence lies here just to check if this file is updated on github//
+    ofSetVerticalSync(true);//ソフトの更新、ディスプレイの更新の頻度を一致させる
+    cam.setOrientation(ofPoint(-20,0,0));
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
+    ofSetFrameRate(30);
+    Basescene * sa = new sceneA();
+    scenes.push_back(sa);
+    Basescene * sb = new sceneB();
+    scenes.push_back(sb);
+    Basescene * sc = new sceneC();
+    //scenes.push_back(sc);
+    //現在シーンを０に。
+    currentScene = 0;
+    scenes[currentScene]->setup();
+    
+    
+    string song_path_list[] = { "GetLucky.mp3","WasHeSlow.mp3","Wannabe.mp3","FirstDate.mp3","WhatYouGot.mp3","GoodGirls.mp3","Get Dat.mp3" };
+//
+//    for (int i =0;  i<songNUM; i++) {
+//        ofSoundPlayer SoundSetup;
+//        SoundSetup.load(song_path_list[i]);
+//        SoundSetup.setVolume(0.5);
+//        SoundSetup.setMultiPlay(false);
+//        SoundSetup.setLoop(true); //loop on by "true"
+//        songs.push_back(SoundSetup);
+//        PlayOrNot.push_back(false);
+//        //配列songsに読み込みとセットアップ
+//    }
+    for (int i=0; i<2; i++) {
+        scenes[i]->setup();
+    }
+    
+    for (int i=0; i<30; i++) {
+        onoff[i]=false;
+    }
+    
+   // songs[1].play();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  scenes[currentScene]->update();
+    handJoint();
+     switch (leap.iGestures) {
+     case 3:
+         {
+             cout<<"swiped/n";
+//             scenes[currentScene]->stopMusic();
+//             currentScene++;
+//             currentScene %=scenes.size();
+//             // scenes[currentScene]->setup();
+//             // scenes[currentScene]->setup();
+//             scenes[currentScene]->startMusic();
+
+         }
+}
+                 leap.updateGestures();  // check for gesture updates
+    leap.markFrameAsOld();
+    if (leap.iGestures ==3) {
+        onoff[ofGetFrameNum()%30]=true;
+    }
+    else   onoff[ofGetFrameNum()%30] = false;
+
+    
+   // onoff[ofGetFrameNum()%30]=false;
+    if (onoff[ofGetFrameNum()%30]==false&&onoff[ofGetFrameNum()%30-1]==true) {
+        scenes[currentScene]->stopMusic();
+        currentScene++;
+        currentScene %=scenes.size();
+        // scenes[currentScene]->setup();
+        scenes[currentScene]->setup();
+        scenes[currentScene]->startMusic();
+        for (int i =0; i<30; i++) {
+            onoff[i]=false;
+        }
+    }
+//    //現在表示しているシーンを更新
+}
+
+void ofApp::handJoint(){
+    simplehands = leap.getsimplehands();
+    if (leap.isFrameNew() &&simplehands.size()) {
+        // fingerPos.clear();
+        handPos.clear();
+        leap.setMappingX(-230, 230, -ofGetWidth()/2, ofGetWidth()/2);
+        leap.setMappingY(90, 490, -ofGetHeight()/2, ofGetHeight()/2);
+        leap.setMappingZ(-150, 150, -200, 200);
+    //    ofVec2f velocity = ofVec2f(LeapCon.palmvelocity, -handPos.y);
+        for (int i =0; i<simplehands.size(); i++) {//指の本数だけ
+            handPos.push_back(simplehands[i].handPos);//手の中心座標をvector配列に追加（手の数だけ行う
+        }
+        
+      
+        }
+}
+    
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+       scenes[currentScene]->draw();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key) {
+        case ' ':
+            scenes[currentScene]->stopMusic();
+            currentScene++;
+            currentScene %=scenes.size();
+           // scenes[currentScene]->setup();
+          // scenes[currentScene]->setup();
+            scenes[currentScene]->startMusic();
+            
+            break;
+
+        case 'f':
+            ofToggleFullscreen();
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ ofApp.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Basescene.hpp"
+#include "sceneA.hpp"
+#include "sceneB.hpp"
+#include "sceneC.hpp"
+#include "ofxLeapMotion2.h"
+
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+        void handJoint();
+        vector <Basescene *> scenes;//sceneA,B,C のインスタンス、ポインタを収納する
+        int currentScene;//現在のシーン番号
+    
+        vector <Boolean> PlayOrNot;
+    //再生する、しないを格納する配列
+      //  vector <ofSoundPlayer> songs ;
+    
+        ofLight light;//陰影を加える
+        ofEasyCam cam; //EasyCamのインスタンス化
+        ofTrueTypeFont font;//文字表示用のインスタンス
+    
+    int songNUM = 6;//収納されている曲の合計数
+    int songID = 0;//曲の通し番号
+    
+    Leap::Controller LeapCon;
+    ofxLeapMotion leap;//LeapMotionのインスタンス化
+    
+    Gesture gesture;
+    vector <ofxLeapMotionSimpleHand> simplehands;//必要に応じてサイズを変えることができるベクター配列
+    vector <ofPoint> handPos;//手の中心位置を保存するvector配列
+    
+    Boolean onoff[30];
+    
+    
+		
+};
